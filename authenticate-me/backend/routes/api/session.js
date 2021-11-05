d501cceda4969ec446c97d59a0d816821a02b080
hauck29 <anthony.hauck@aol.com>
2021-11-04 21:37:38 -0700

    added restore user functionality to user auth

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -34,4 +34,14 @@ router.delete(
     }
   );
 
+//Restore Session User
+router.get('/', restoreUser, (req, res) => {
+    const {user} = req;
+    if(user) {
+        return res.json({
+            user:user.toSafeObject()
+        });
+    }else return res.json({});
+})
+
 module.exports = router;
