4f16551f85dd18b0d8bb345114b6072f1627bff6
hauck29 <anthony.hauck@aol.com>
2021-11-08 09:44:21 -0800

    im dumb comment, its true

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
     );
   });
 }
-
+//lol, im dumb
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
