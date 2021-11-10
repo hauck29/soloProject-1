6f5ce08e73f62ad47541d42ef774f45899f0246e
hauck29 <anthony.hauck@aol.com>
2021-11-09 16:42:15 -0800

    removed .env

--- backend/routes/api/questions.js
+++ backend/routes/api/questions.js
@@ -13,7 +13,14 @@ router.post('/', asyncHandler(async(req, res) => {
     const {ownerId, title, description} = req.body;
     const newQ = await Question.create({ownerId, title, description});
     return res.json({newQ});
+}));
 
-}))
+router.delete('/:id(\\d+)', async (req, res, next) => {
+    const question = await Question.findByPk(req.params.id);
+    if(question) {
+        await question.destroy();
+        res.status(204).end();
+    }
+})
 
 module.exports = router;
