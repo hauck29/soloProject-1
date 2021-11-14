42e0084678f525cbe801811bdf49f785b970db6e
hauck29 <anthony.hauck@aol.com>
2021-11-14 10:17:41 -0800

    added answer validators

--- frontend/src/components/AnswersFeed/index.js
+++ frontend/src/components/AnswersFeed/index.js
@@ -3,12 +3,12 @@ import { useDispatch } from "react-redux";
 import { removeAnswer } from "../../store/answers";
 import { useHistory } from "react-router";
 import { editAnswer } from "../../store/answers";
+import "./AnswersFeed.css";
 
 const Answers = ({ answer }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [editAnswer, setEditAnswer] = useState(false);
-
 
   const handleDelete = (answer) => {
     dispatch(removeAnswer(answer));
@@ -18,7 +18,7 @@ const Answers = ({ answer }) => {
   const handleEditASubmit = (e) => {
     // e.preventDefault();
     const payload = {
-      id: answer.id
+      id: answer.id,
     };
     dispatch(editAnswer(payload));
     setEditAnswer(!editAnswer);
@@ -27,9 +27,13 @@ const Answers = ({ answer }) => {
   return (
     <div>
       <div className="answers">
-        <p>{answer.answer}</p>
-        <button onClick={() => handleDelete(answer)}>Delete</button>
-        <button onClick={() => handleEditASubmit(answer)}>Edit</button>
+        <div className="answerText">
+          <p>{answer.answer}</p>
+        </div>
+        <div className="answerBtns">
+          <button onClick={() => handleDelete(answer)}>Delete</button>
+          <button onClick={() => handleEditASubmit(answer)}>Edit</button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/CreateAnswer/index.js
+++ frontend/src/components/CreateAnswer/index.js
@@ -1,57 +1,68 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
-import { addAnswer } from '../../store/answers';
-import './CreateAnswer.css';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { addAnswer } from "../../store/answers";
+import "./CreateAnswer.css";
 
 const CreateAnswer = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const { questionId } = useParams();
 
-    const sessionUser = useSelector((state) => state.session.user);
-    const {questionId} = useParams();
+  // const [questionId, setQuestionId] = useState('');
+  const [answer, setAnswer] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
 
+  // const user = useSelector(state => state.session.user);
 
-    // const [questionId, setQuestionId] = useState('');
-    const [answer, setAnswer] = useState('');
-    const history = useHistory();
-    const dispatch = useDispatch();
-    // const user = useSelector(state => state.session.user);
+  const cancel = (e) => {
+    e.preventDefault();
+    history.push("/");
+  };
 
-    const cancel = (e) => {
-        e.preventDefault();
-        history.push('/');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (answer.length < 1) {
+      setErrors(["You must enter in a value to post it as an answer!"]);
+      return errors;
     }
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const payload = {
-            userId: sessionUser.id,
-            questionId,
-            answer
-        };
-        dispatch(addAnswer(payload));
-
-        history.push('/');
+    const payload = {
+      userId: sessionUser.id,
+      questionId,
+      answer,
     };
+    dispatch(addAnswer(payload));
 
-    return (
-        <div className='add-answer'>
-            <h3>Add An Answer</h3>
-            <form onSubmit={handleSubmit} >
+    setTimeout(() => {
+      history.push("/");
+    }, 250);
+  };
 
-                <input
-                    onChange={(e) => setAnswer(e.target.value)}
-                    value={answer}
-                    placeholder='Enter your answer here'
-                />
-                <div className='add-a-btns'>
-                    <button className='answer-sumbit-btn' type='submit'>
-                        Add Answer
-                    </button>
-                    <button className='cancel-btn' onClick={cancel}>Cancel</button>
-                </div>
-            </form>
+  return (
+    <div className="add-answer">
+      <h3>Add An Answer</h3>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setAnswer(e.target.value)}
+          value={answer}
+          placeholder="Enter your answer here"
+        />
+        <div className="add-a-btns">
+          <button className="answer-sumbit-btn" type="submit">
+            Add Answer
+          </button>
+          <button className="cancel-btn" onClick={cancel}>
+            Cancel
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 export default CreateAnswer;
